13350163e314e92022ef3a827a5b3e0717a2d689
Philip White <phil_23@live.co.uk>
2019-10-31 22:09:30 +0000

    Add react router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import { CssBaseline, Button } from "@material-ui/core";
 import { Home } from "@material-ui/icons";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const theme = createMuiTheme({});
 
@@ -9,14 +10,31 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <ThemeProvider theme={theme}>
-        <div>
-          <Home color="primary" fontSize="large" />
-          <Button variant="contained" color="primary">
-            Hello World
-          </Button>
-        </div>
-      </ThemeProvider>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/registers">Registers</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/registers">Registers</Route>
+              <Route path="/">
+                <Home color="primary" fontSize="large" />
+                <Button variant="contained" color="primary">
+                  Hello World
+                </Button>
+              </Route>
+            </Switch>
+          </div>
+        </ThemeProvider>
+      </Router>
     </>
   );
 }
