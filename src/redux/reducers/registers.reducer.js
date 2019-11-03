358fe5b06655f1f8f7c4f7018394a291948cca8e
Philip White <phil_23@live.co.uk>
2019-11-03 19:03:37 +0000

    Normalize resisters reducer state

--- src/redux/reducers/registers.reducer.js
+++ src/redux/reducers/registers.reducer.js
@@ -1,23 +1,31 @@
 import { GET_REGISTERS_SUCCESS, ADD_REGISTER_SUCCESS } from "redux/actions";
 
-const initialState = {};
+const initialState = {
+  byId: {},
+  allIds: []
+};
 
 const registers = (state = initialState, action) => {
   switch (action.type) {
     case GET_REGISTERS_SUCCESS:
-      return action.payload.reduce((acc, x) => ({ ...acc, [x.id]: x }), {});
+      return {
+        byId: action.payload.reduce((acc, x) => ({ ...acc, [x.id]: x }), {}),
+        allIds: action.payload.map(x => x.id)
+      };
     case ADD_REGISTER_SUCCESS:
       return {
-        ...state,
-        [action.payload.id]: action.payload
+        byId: {
+          ...state.byId,
+          [action.payload.id]: action.payload
+        },
+        allIds: [...state.allIds, action.payload.id]
       };
     default:
       return state;
   }
 };
 
-export const selectRegisters = state => ({
-  registers: Object.keys(state.registers).map(key => state.registers[key])
-});
+export const selectRegisters = state =>
+  state.registers.allIds.map(id => state.registers.byId[id]);
 
 export default registers;
--- src/redux/reducers/registers.reducer.test.js
+++ src/redux/reducers/registers.reducer.test.js
@@ -1,25 +1,35 @@
-import reducer from "./registers.reducer";
+import reducer, { selectRegisters } from "./registers.reducer";
 import { getRegistersSuccess, addRegisterSuccess } from "redux/actions";
 
 describe("Registers Reducer", () => {
   it("Has the correct initial state", () => {
-    const expectedState = {};
+    const expectedState = {
+      byId: {},
+      allIds: []
+    };
 
     expect(reducer(undefined, { type: "" })).toEqual(expectedState);
   });
 
   it("GET_REGISTERS_SUCCESS replaces the registers state", () => {
     const initialState = {
-      3: { id: 3, name: "THREE" }
+      byId: {
+        3: { id: 3, name: "THREE" }
+      },
+      allIds: [3]
     };
+
     const action = getRegistersSuccess([
       { id: 1, name: "ONE" },
       { id: 2, name: "TWO" }
     ]);
 
     const expectedState = {
-      1: { id: 1, name: "ONE" },
-      2: { id: 2, name: "TWO" }
+      byId: {
+        1: { id: 1, name: "ONE" },
+        2: { id: 2, name: "TWO" }
+      },
+      allIds: [1, 2]
     };
 
     expect(reducer(initialState, action)).toEqual(expectedState);
@@ -27,16 +37,41 @@ describe("Registers Reducer", () => {
 
   it("ADD_REGISTER_SUCCESS adds an additional register", () => {
     const initialState = {
-      1: { id: 1, name: "ONE" }
+      byId: {
+        1: { id: 1, name: "ONE" }
+      },
+      allIds: [1]
     };
 
     const action = addRegisterSuccess({ id: 2, name: "TWO" });
 
     const expectedState = {
-      1: { id: 1, name: "ONE" },
-      2: { id: 2, name: "TWO" }
+      byId: {
+        1: { id: 1, name: "ONE" },
+        2: { id: 2, name: "TWO" }
+      },
+      allIds: [1, 2]
     };
 
     expect(reducer(initialState, action)).toEqual(expectedState);
+  });
+
+  describe("Selectors", () => {
+    it("selectRegisters returns all registers as an array", () => {
+      const state = {
+        registers: {
+          byId: {
+            1: { id: 1, name: "ONE" },
+            2: { id: 2, name: "TWO" }
+          },
+          allIds: [1, 2]
+        }
+      };
+
+      expect(selectRegisters(state)).toEqual([
+        { id: 1, name: "ONE" },
+        { id: 2, name: "TWO" }
+      ]);
+    });
   });
 });
